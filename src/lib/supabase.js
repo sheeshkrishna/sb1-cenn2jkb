797780cc2fd95277e6005aa5337f4464130b1fcf
Krishnasheesh Datta <126602314+sheeshkrishna@users.noreply.github.com>
2025-09-18 16:54:30 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl    = 'https://bnxarztuttbjvjkypnde.supabase.co'
-const supabaseAnonKey= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueGFyenR1dHRianZqa3lwbmRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNzE4OTAsImV4cCI6MjA3Mzc0Nzg5MH0.Hp9HqTnBfGa_R8ypOJ-YxnC424OvEcYirgdqXXspTtY'
+const supabaseUrl     = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
